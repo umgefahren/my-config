b0892dbae0e1fbcad0d7f71dff497dba615d1fa5
umgefahren <55623006+umgefahren@users.noreply.github.com>
2022-08-21 14:58:52 +0200

    added possibility to pass per command line argument

--- go/unicode/main.go
+++ go/unicode/main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
+var str = flag.String("s", "", "You can, alternativly, pass a string directly")
 
-func main() {
-        if len(os.Args) != 2 {
-                fmt.Println("Pass one argument")
-        }
-        arg := os.Args[1]
-        for _, r := range arg {
+func PrintUnicode(inp string) {
+        for _, r := range inp {
                 fmt.Printf("Rune: %s Unicode: %U\n", string(r), r)
         }
+}
+
+func main() {
+        flag.Parse()
+        if *str != "" {
+                PrintUnicode(*str)
+                return
+        }
+        if len(os.Args) != 2 {
+                fmt.Println("Pass one argument")
+                return 
+        }
+        arg := os.Args[1]
+        PrintUnicode(arg)
 }
